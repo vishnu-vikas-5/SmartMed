10a77ce458364981ee1a8befbead85d970092e24
vishnu vikas <vishnuvikas@Vishnus-MacBook-Air.local>
2026-09-15 21:34:25 +0530

    Configure Android Gradle desugaring & google-services for APK build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.google.gms.google-services")
 }
 dependencies {
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
   // Import the Firebase BoM
   implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
 
@@ -22,6 +23,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
